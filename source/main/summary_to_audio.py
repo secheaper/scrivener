48956ece7a488f14261048d800436dacf6f85045
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-02 15:55:27 -0400

    added license text to each code file

--- source/main/summary_to_audio.py
+++ source/main/summary_to_audio.py
@@ -1,3 +1,11 @@
+"""
+Copyright (C) 2021 SE Transcriptor - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the MIT license.
+You should have received a copy of the MIT license with
+this file. If not, please write to: secheaper@gmail.com
+"""
+
 from gtts import gTTS
 
 class toAudio():
