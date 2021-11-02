c04346e0a465fc58946aa375ecea6a3b509dc291
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-02 15:56:28 -0400

    added MIT license to each code fide in helper folder:

--- source/helper/__init__.py
+++ source/helper/__init__.py
@@ -1,5 +1,7 @@
 """
-@author: Scrivener
+Copyright (C) 2021 SE Transcriptor - All Rights Reserved
+You may use, distribute and modify this code under the
+terms of the MIT license.
+You should have received a copy of the MIT license with
+this file. If not, please write to: secheaper@gmail.com
 """
-
-
