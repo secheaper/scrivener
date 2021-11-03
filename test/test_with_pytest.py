fd1f4dab6a17bc6b90ba51520ff9b7030119d307
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-03 10:29:27 -0400

    added sentiment test

--- test/test_with_pytest.py
+++ test/test_with_pytest.py
@@ -1,7 +1,18 @@
-# test_with_pytest.py
+import sys
+# insert at 1, 0 is the script path (or '' in REPL)
+sys.path.append('source/main')
+
+import helper
 
 def test_always_passes():
     assert True
 
+def test_analyzeText():
+    """
+    Checks the analyze function
+    """
+    sentiments = ["Neutral", "Positive", "Negative"]
+    assert helper.analyze("sad")[0] in sentiments
+   
 # def test_always_fails():
 #     assert False
