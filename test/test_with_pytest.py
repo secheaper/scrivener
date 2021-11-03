9326a9a2d79bf5964dde8d094437143e45929ea1
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-03 15:00:47 -0400

    added tests for TranscribeYT and Summary

--- test/test_with_pytest.py
+++ test/test_with_pytest.py
@@ -2,7 +2,9 @@ import sys
 # insert at 1, 0 is the script path (or '' in REPL)
 sys.path.append('source/main')
 
-import helper
+from main import helper
+from main import summarize
+from main.transcribe_yt import TranscribeYtVideo
 
 def test_always_passes():
     assert True
@@ -25,5 +27,13 @@ def test_analyzeText():
     assert helper.analyze("very happy")[0] in sentiments
     assert helper.analyze("decent")[0] in sentiments
    
+# from main.summarize import Summary
+
+def test_Summary():
+    assert type(summary.Summarize("the snail smelly old beermongers . the snail stinky old beersmonger's squirmy old")) == str
+
+def test_transcribeVideo():
+    assert type(TranscribeYtVideo('https://www.youtube.com/watch?v=yaG9mFlrB8k&ab_channel=9to5Mac').transcribe_yt_video()) == str
+
 # def test_always_fails():
 #     assert False
