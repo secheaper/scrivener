cf93155dca289464ca69cef6b36e4feb499ff5d5
Rahil Sarvaiya <rahil1304@gmail.com>
2021-11-03 11:33:54 -0400

    added none type test

--- test/test_with_pytest.py
+++ test/test_with_pytest.py
@@ -8,19 +8,22 @@ def test_always_passes():
     assert True
 
 def test_formatSummary():
-    print(helper.formatText("I am very sad"))
-    # assert helper.formatText("sad") == type(str)
+    """
+    Checks the formatText function
+    """
+    # print(helper.formatText(""))
+    # assert helper.formatText("")[-1] is None
+    assert helper.formatText("Robert Frost is taking the right road to summarize your video")[-1] == '.'
 
 def test_analyzeText():
     """
     Checks the analyze function
     """
     sentiments = ["Neutral", "Positive", "Negative"]
+    assert helper.analyze("")[0] == None
     assert helper.analyze("sad")[0] in sentiments
     assert helper.analyze("very happy")[0] in sentiments
     assert helper.analyze("decent")[0] in sentiments
-
-
    
 # def test_always_fails():
 #     assert False
